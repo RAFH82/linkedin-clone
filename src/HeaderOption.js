6f5afee9980d123fb9d4dd43f7e61ac96daa5360
RAFH82 <hrechkar@gmail.com>
2021-02-02 14:38:30 -0800

    Added avatar img and Material-UI makeStyles()

--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./HeaderOption.css";
+import { Avatar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-function HeaderOption({ Icon, title }) {
+// Material UI custom styles
+const useStyles = makeStyles({
+	headerOption__icon: {
+		objectFit: "contain",
+		height: 25,
+		width: 25,
+	},
+});
+
+function HeaderOption({ avatar, Icon, title }) {
+	const classes = useStyles();
 	return (
 		<div className="headerOption">
-			{Icon && <Icon className="headerOption__icon" />}
+			{Icon && <Icon className={classes.headerOption__icon} />}
+			{avatar && <Avatar className={classes.headerOption__icon} src={avatar} />}
 			<h3 className="headerOption__title">{title}</h3>
 		</div>
 	);
