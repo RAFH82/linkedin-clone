1fbdc5fb227a61fdd9532569fbcfbdaa5e0a70b1
RAFH82 <hrechkar@gmail.com>
2021-02-02 14:10:26 -0800

    building header cpmponent

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,14 @@
 import React from "react";
+import HeaderOption from "./HeaderOption";
 import "./Header.css";
+
+// Material UI
 import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function Header() {
 	return (
@@ -12,10 +20,16 @@ function Header() {
 				/>
 				<div className="header__search">
 					<SearchIcon />
-					<input type="text" />
+					<input type="text" placeholder="Search" />
 				</div>
 			</div>
-			<div className="header__right"></div>
+			<div className="header__right">
+				<HeaderOption Icon={HomeIcon} title="Home" />
+				<HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+				<HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+				<HeaderOption Icon={ChatIcon} title="Messaging" />
+				<HeaderOption Icon={NotificationsIcon} title="Notifications" />
+			</div>
 		</div>
 	);
 }
