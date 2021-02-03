2ffdd026ee7862c4e0cbc43d7fe6cb177f4d73d8
RAFH82 <hrechkar@gmail.com>
2021-02-02 20:35:00 -0800

    styling input options

--- src/Feed.js
+++ src/Feed.js
@@ -1,7 +1,11 @@
-import { createSvgIcon } from "@material-ui/core";
 import React from "react";
 import "./Feed.css";
 import CreateIcon from "@material-ui/icons/Create";
+import ImageIcon from "@material-ui/icons/Image";
+import InputOption from "./InputOption";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
 
 function Feed() {
 	return (
@@ -17,6 +21,16 @@ function Feed() {
 						/>
 						<button type="submit">Send</button>
 					</form>
+				</div>
+				<div className="feed__inputOptions">
+					<InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+					<InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+					<InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+					<InputOption
+						Icon={CalendarViewDayIcon}
+						title="Write Article"
+						color="7FC15E"
+					/>
 				</div>
 			</div>
 		</div>
