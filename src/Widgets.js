fe6f5ff45dbbc3bcfe314af124c14c0a00886afa
RAFH82 <hrechkar@gmail.com>
2021-02-04 15:54:10 -0800

    Completed widgets section

--- src/Widgets.js
+++ src/Widgets.js
@@ -2,11 +2,22 @@ import React from "react";
 import "./Widgets.css";
 import InfoIcon from "@material-ui/icons/Info";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import { makeStyles } from "@material-ui/core/styles";
+
+// Material UI custom styles
+const useStyles = makeStyles({
+	widget__articleIcon: {
+		fontSize: 12,
+	},
+});
+
 function Widgets() {
+	const classes = useStyles();
+
 	const newsArticle = (heading, subtitle) => (
 		<div className="widgets__article">
 			<div className="widgets__articleLeft">
-				<FiberManualRecordIcon />
+				<FiberManualRecordIcon className={classes.widget__articleIcon} />
 			</div>
 			<div className="widgets__articleRight">
 				<h4>{heading}</h4>
@@ -23,16 +34,16 @@ function Widgets() {
 			</div>
 			{newsArticle(
 				"Ryan Hrechka graduates Lighthouse Labs!",
-				"Now seeking job opportunities to build new exciting things!"
+				"Canada News - 4325 readers"
 			)}
-			{newsArticle(
-				"Corona Virus Canada updates",
-				"Cases dropping due to stay at home orders"
-			)}
+			{newsArticle("Corona Virus Canada update", "Top news - 1424 readers")}
 			{newsArticle("Bitcon hits new ath of $22k", "Crypto - 8000 readers")}
+			{newsArticle("Learn to code!", "Learn to Code - 324 readers")}
+			{newsArticle("Is Redux any good?", "Code - 123 readers")}
+			{newsArticle("Tesla hits new highs", "Car & Auto - 389 readers")}
 			{newsArticle(
-				"Learn to code!",
-				"Anyone can do it, with time, patience, and practice!"
+				"Is RAFH the best island DJ?",
+				"Music & Entertainment - 564  readers"
 			)}
 		</div>
 	);
