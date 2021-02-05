5b6ea20649fdc74ce4891fa807639429af2a88e7
RAFH82 <hrechkar@gmail.com>
2021-02-04 16:06:02 -0800

    Updated avatar display when not logged in

--- src/Header.js
+++ src/Header.js
@@ -40,11 +40,15 @@ function Header() {
 				<HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
 				<HeaderOption Icon={ChatIcon} title="Messaging" />
 				<HeaderOption Icon={NotificationsIcon} title="Notifications" />
-				<HeaderOption
-					avatar={true}
-					title={user?.displayName}
-					onClick={logoutOfApp}
-				/>
+				{!user ? (
+					<></>
+				) : (
+					<HeaderOption
+						avatar={true}
+						title={user?.displayName}
+						onClick={logoutOfApp}
+					/>
+				)}
 			</div>
 		</div>
 	);
